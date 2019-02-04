db9cf843442744b4faf54a8c25a5ef7ee85e89bc
rahib <rsayed@spartaglobal.com>
2019-02-04 16:02:06 +0000

    Comparing the selected word to the unknown words, then removing the found word from the array

--- js/index.js
+++ js/index.js
@@ -5,28 +5,54 @@ $(document).ready(function() {
     var setwords = ["NOT","OUT","NUT","UNTO"];
     var letters = ["N","O", "U", "T"];
     var word_select = "";
+    $(letters).each(function(index, letter){
+      $("#words").append("<li>" + letter + "</li>");
+    });
 
     link_words();
 
     function link_words(){
 
-      $(letters).each(function(index, letter){
-        $("#words").append("<li>" + letter + "</li>");
-      });
-
-
         $("li").click(function(){
-          while ("li" !== letters.length) {
-          word_select = $("<h2> "+ this.innerHTML +" </h2>");
+          word_select += String(this.innerText);
           $(".display_letters").html(word_select);
-          }
+          console.log(word_select);
+          checkWord(word_select);
         });
 
+
+        $("button").click(function(){
+        word_select = clearButton(word_select);
+        });
+
+      }
+
+
+    function clearButton(word){
+        word = "";
+        $(".display_letters").html(word);
+        return word;
     }
 
-    function checkWords(){
-
+    function splitWord(word){
+       for (var i = 0; i < word.length; i++) {
+          return word.charAt(i);
+         }
     }
+
+    function checkWord(word){
+      for (var i = 0; i < setwords.length; i++) {
+        if (word == setwords[i]){
+          word = clearButton(word);
+          setwords.splice(i, word);
+          // remove word from array
+//          display_word(word);
+        }
+    }
+  }
+
+  function display_word(display){
 
   }
+}
 });

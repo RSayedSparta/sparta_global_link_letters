c11d2d4d2a20b529451096f3ff3d40925e2c9971
rahib <rsayed@spartaglobal.com>
2019-02-05 00:46:30 +0000

    Able to remove words on the array when finding a word

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 
   init();
 
-//   var items = [
-//   [" ", " ", " ", " "],
-//   [" ", " ", " ", " "],
-//   [" ", " ", " ", " "],
-//   [" ", " ", " ", " "]
-// ];
+  //   var items = [
+  //   [" ", " ", " ", " "],
+  //   [" ", " ", " ", " "],
+  //   [" ", " ", " ", " "],
+  //   [" ", " ", " ", " "]
+  // ];
 
   function init(){
     var setwords = ["NOT","OUT","NUT","UNTO"];
@@ -21,47 +21,51 @@ $(document).ready(function() {
 
     function link_words(){
 
-        $("li").click(function(){
-          word_select += String(this.innerText);
-          $(".display_letters").html(word_select);
-          console.log(word_select);
-          checkWord(word_select);
-        });
+      $("li").click(function(){
+        word_select += String(this.innerText);
+        $(".display_letters").html(word_select);
+        console.log(word_select);
+        word_select = checkWord(word_select);
+        console.log(setwords);
+      });
 
 
-        $("button").click(function(){
+      $("button").click(function(){
         word_select = clearButton(word_select);
-        });
+      });
 
-      }
+    }
 
 
     function clearButton(word){
-        word = "";
-        $(".display_letters").html(word);
-        return word;
+      word = "";
+      $(".display_letters").html(word);
+      return word;
     }
 
-    function splitWord(word){
-       for (var i = 0; i < word.length; i++) {
-          return word.charAt(i);
-         }
-    }
+    // function splitWord(word){
+    //   for (var i = 0; i < word.length; i++) {
+    //     return word.charAt(i);
+    //   }
+    // }
 
     function checkWord(word){
       for (var i = 0; i < setwords.length; i++) {
         if (word == setwords[i]){
-          setwords.splice(i, word);
+          setwords.splice( setwords.indexOf(word), 1 );
+          //Find the index position of the current "word" then remove one element from that position
           word = clearButton(word);
+          return word;
           // reread splice
           // remove word from array
-//          display_word(word);
+          // display_word(word);
         }
+      }
     }
-  }
 
-  function display_word(display){
+    function display_word(display){
+
+    }
 
   }
-}
 });
